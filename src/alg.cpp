710d03ee33ec1150ad8b952302ae62d3908ddabc
abramovayulia <116936584+abramovayulia@users.noreply.github.com>
2023-02-26 22:54:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,13 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-   if (a == 0 || b == 0) {
-    return a+b;
-  }
-   if (a > b) {
-     return gcd(a - b, b);
-   } else {
-     return gcd(a, b - a);
+     if (a == b) {
+    return a;
+  } else {
+    if (a > b) {
+      return gcd(a - b, b);
+    } else {
+      return gcd(a, b - a);
     }
   }
+}
